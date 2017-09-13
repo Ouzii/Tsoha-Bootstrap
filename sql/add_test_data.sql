80a85989f41434417609b10be29d83a5328200de
laajaosk <laajaosk@helsinki.fi>
2017-09-13 17:01:32 +0300

    Lisätty tietokantalauseet

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,27 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+
+INSERT INTO Kayttaja (tunnus, salasana, ika, kuvaus, admin)
+VALUES ('Matti', 'salasana', 23, 'Matti Meikäläinen', TRUE);
+
+INSERT INTO Kayttaja (tunnus, salasana, ika, kuvaus)
+VALUES ('Maija', 'Kissa123', 24, 'Maija Meikäläinen');
+
+INSERT INTO Tyon_kohde (kuvaus, tarkempi_kuvaus)
+VALUES ('Jääkaappi', 'Keittiöstä löytyvä kaappi, joka pitää ruoat kylmänä.');
+
+INSERT INTO Tyon_kohde (kuvaus, tarkempi_kuvaus)
+VALUES ('Olohuone', 'Talon ensimmäisestä kerroksesta löytyvä olohuone.');
+
+INSERT INTO Tyokalu (kuvaus, tarkempi_kuvaus)
+VALUES ('Rätti', 'Keittiön kaapista löytyvä punainen rätti. Käytetään pintojen pyyhkimiseen.');
+
+INSERT INTO Tyokalu (kuvaus, tarkempi_kuvaus)
+VALUES ('Imuri', 'Eteisen kaapista löytyvä Bosch-merkkinen imuri.');
+
+INSERT INTO Tyo (kohde, tyokalu, tekija, kuvaus, tarkempi_kuvaus, tehty, suoritusaika)
+VALUES ('Jääkaappi', 'Rätti', 'Matti', 'Siivoa jääkaappi rätillä.',
+ 'Keittiöstä löytyy sekä punainen rätti, että jääkaappi. Pyyhi jääkaapin pinnat rätillä.', TRUE, NOW());
+
+INSERT INTO Tyo (kohde, tyokalu, tekija, kuvaus, tarkempi_kuvaus)
+VALUES ('Olohuone', 'Imuri', 'Maija', 'Imuroi olohuone.',
+ 'Imuroi olohuone imurilla. Imuri löytyy eteisen kaapista. Muista myös sohvan alta!');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,31 @@
 -- Lisää CREATE TABLE lauseet tähän tiedostoon
+
+CREATE TABLE Kayttaja(
+	tunnus varchar(10) PRIMARY KEY,
+	salasana varchar(20) NOT NULL,
+	ika INTEGER,
+	kuvaus varchar(240),
+	admin boolean
+);
+
+CREATE TABLE Tyon_kohde(
+	kuvaus varchar(30) PRIMARY KEY,
+	tarkempi_kuvaus varchar(240),
+	luotu TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Tyokalu(
+	kuvaus varchar(30) PRIMARY KEY,
+	tarkempi_kuvaus varchar(240),
+	luotu TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
+
+CREATE TABLE Tyo(
+	kohde varchar REFERENCES Tyon_kohde(kuvaus),
+	tyokalu varchar REFERENCES Tyokalu(kuvaus),
+	tekija varchar REFERENCES Kayttaja(tunnus),
+	kuvaus varchar(30) NOT NULL,
+	tarkempi_kuvaus varchar(240),
+	tehty boolean DEFAULT FALSE,
+	suoritusaika TIMESTAMP
+);
